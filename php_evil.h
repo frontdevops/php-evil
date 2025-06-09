fa3996361f85628cb1b040c182213ba012159b09
Sasha Majorović <alexander.majorov@gmail.com>
2025-06-09 13:16:50 +0200

    Update php_evil.h

--- php_evil.h
+++ php_evil.h
@@ -6,7 +6,7 @@
 extern zend_module_entry evil_module_entry;
 # define phpext_evil_ptr &evil_module_entry
 
-# define PHP_EVIL_VERSION "0.1.0"
+# define PHP_EVIL_VERSION "0.2.0"
 
 # if defined(ZTS) && defined(COMPILE_DL_EVIL)
 ZEND_TSRMLS_CACHE_EXTERN()
